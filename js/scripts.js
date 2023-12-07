91ea9889e2fb6a26a9592fb1e7f0cde2463c93b2
igl-myrick <igl.myrick@gmail.com>
2023-12-07 14:12:40 -0800

    add html and ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,19 @@ function filterOffensiveWords(text) {
   });
   return inputArray.join(" ");
 };
+
+// ui logic
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
